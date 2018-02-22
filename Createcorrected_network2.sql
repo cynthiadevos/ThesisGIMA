4bb5d541cd712d3e9ae43398b9ebe177b36340d6
cynthiadevos <30632917+cynthiadevos@users.noreply.github.com>
2018-02-22 15:31:09 +0100

    Add files via upload

---
+++ Createcorrected_network2.sql
@@ -0,0 +1,15 @@
+-- Create table that only contains the keys with the shortest length
+
+SELECT key as key2, min(shape_leng) as lengteinm
+INTO links_zui_corr3_extra
+FROM links_zui_corr3 
+GROUP BY key 
+
+-- Create shapefile that only contains the keys with the shortest length 
+
+pgsql2shp <requirements> "<query>"
+
+SELECT * 
+FROM links_zui_corr3 AS b 
+INNER JOIN links_zui_corr3_extra AS e 
+ON b.key = e.key2 AND b.shape_leng = e.lengteinm
